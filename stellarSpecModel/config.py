cc6f52fa6a8fc256712eca197a7df753531bb7f9
Matthew O'Callaghan <33609307+mattocallaghan@users.noreply.github.com>
2025-05-27 10:28:43 +0100

    Update config.py

--- stellarSpecModel/config.py
+++ stellarSpecModel/config.py
@@ -4,6 +4,7 @@ from hashlib import md5
 
 
 home_dir = os.get_cwd()
+print(home_dir)
 grid_data_dir = os.getenv('stellarSpecModel_grid_PATH', f'{home_dir}/.stellarSpecModel/grid_data/')
 
 grid_names = {
